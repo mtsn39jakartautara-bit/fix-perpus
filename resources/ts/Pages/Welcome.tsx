13db361853425f196dba28484ff5bc5012ec222b
rhaihanadzani <mrhaihan17@gmail.com>
2026-02-23 14:15:25 +0700

    update:cleaing code

--- resources/ts/Pages/Welcome.tsx
+++ resources/ts/Pages/Welcome.tsx
@@ -1,34 +1,13 @@
-import CallToAction from "@/Components/home/CallToAction";
-import Hero from "@/Components/home/Hero";
-import LatestNews from "@/Components/home/LatestNews";
-import Partners from "@/Components/home/Partners";
-import Stats from "@/Components/home/Stats";
-import Testimonials from "@/Components/home/Testimonials";
-import Testing from "@/Components/home/Testing";
-import AppShell from "@/Components/layouts/AppShell";
-import { Link, Head } from "@inertiajs/react";
-import { Footer } from "react-day-picker";
-import { route } from "ziggy-js";
+import { Head } from "@inertiajs/react";
 
 export default function Welcome(props: any) {
     return (
         <>
             <Head title="Welcome" />
-            <AppShell>
-                <div className="min-h-screen">
-                    {/* <Header /> */}
-                    <main>
-                        <Hero />
-                        <Testing />
-                        {/* <Stats /> */}
-                        <LatestNews />
-                        <Testimonials />
-                        <Partners />
-                        {/* <CallToAction /> */}
-                    </main>
-                    <Footer />
-                </div>
-            </AppShell>
+
+            <main>
+                <h1 className="text-3xl">halo</h1>
+            </main>
         </>
     );
 }
